eb569f9f1eb581a184eacba7382766d43a0605d7
Kapil Jugnu <kr_kapil@hotmail.com>
2024-05-25 12:40:50 +0530

    move js script to app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import { is_loggedin, supabase_login, supabase_logout } from './services/app_auth';
 import { insert_user_location } from './services/geolocation';
 import { fetch_country, update_country, delete_country, create_country } from './services/countries';
+import { makeScriptsExecutable, render_app, render_test } from './services/render_app';
 
 window.is_loggedin = is_loggedin;
 window.supabase_login = supabase_login;
@@ -12,3 +13,14 @@ window.db_op = {
     delete_country,
     create_country
 }
+window.makeScriptsExecutable = makeScriptsExecutable;
+window.render_app = render_app;
+window.render_test = render_test;
+
+
+
+const go = new Go();
+WebAssembly.instantiateStreaming(fetch("main.wasm"), go.importObject).then((result) => {
+    go.run(result.instance);
+    is_loggedin()
+});
